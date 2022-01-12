fbf7ad8707d171e89f172251e9de0308845fe924
Santiago Miqueo <smiqueo@hotmail.com>
2022-01-12 20:34:37 -0300

    Meta, SEO

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 import "animate.css"
+import { Helmet } from "react-helmet"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -10,11 +11,55 @@ import Slider from "../components/slider"
 import About from "../components/about"
 
 const IndexPage = () => (
-  <Layout>
-    <Form />
-    <Slider />
-    <About />
-  </Layout>
+  <div>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <link rel="canonical" href="http://mysite.com/example" />
+
+      {/* <!-- Primary Meta Tags --> */}
+      <title>FLORENCIA FRANCO - Timeless portraiture</title>
+      <meta name="title" content="FLORENCIA FRANCO - Timeless portraiture" />
+      <meta
+        name="description"
+        content="This website is so awesome that I really don't think you can handle how much awesome that is happening here."
+      />
+      {/* <!-- Open Graph / Facebook --> */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://florenciafranco.com/" />
+      <meta
+        property="og:title"
+        content="FLORENCIA FRANCO - Timeless portraiture"
+      />
+      <meta
+        property="og:description"
+        content="This website is so awesome that I really don't think you can handle how much awesome that is happening here."
+      />
+      <meta
+        property="og:image"
+        content="https://florenciafranco.com/facebookimage.jpg"
+      />
+      {/* <!-- Twitter --> */}
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://florenciafranco.com/" />
+      <meta
+        property="twitter:title"
+        content="FLORENCIA FRANCO - Timeless portraiture"
+      />
+      <meta
+        property="twitter:description"
+        content="This website is so awesome that I really don't think you can handle how much awesome that is happening here."
+      />
+      <meta
+        property="twitter:image"
+        content="https://florenciafranco.com/twitterimage.jpg"
+      />
+    </Helmet>
+    <Layout>
+      <Form />
+      <Slider />
+      <About />
+    </Layout>
+  </div>
 )
 
 export default IndexPage
